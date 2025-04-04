f18d2c93c0418a229f5a2d7e11184079df7efd7f
Sahil-Singh23 <sahil091205@gmail.com>
2025-04-04 23:50:57 +0530

    new changes

--- starter/script.js
+++ starter/script.js
@@ -1,2 +1,10 @@
-//document object model
+//document object model manipulation
 'use strict';
+
+console.log(document.querySelector('.message').textContent);
+//we are selecting here using dom
+document.querySelector('.message').textContent = 'Correct';
+
+document.querySelector('.score').textContent = '13';
+
+document.querySelector();
